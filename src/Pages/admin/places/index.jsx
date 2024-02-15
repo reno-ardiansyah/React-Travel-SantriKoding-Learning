4237caec8c24ecab9dce57441f282df35c162a93
Reno <144994054+RenoArdiansyah@users.noreply.github.com>
2024-02-15 23:28:09 +0700

    add Index, Create Category admin api

--- src/Pages/admin/places/index.jsx
+++ src/Pages/admin/places/index.jsx
@@ -1,111 +1,62 @@
-//import react  
 import React, { useState, useEffect } from "react";
-
-//import layout admin
 import LayoutAdmin from "../../../layouts/Admin";
-
-//import BASE URL API
 import Api from "../../../api";
-
-//import js cookie
 import Cookies from "js-cookie";
-
-//import Link from react router dom
 import { Link } from "react-router-dom";
-
-//import pagination component
 import PaginationComponent from "../../../components/utilities/Pagination";
-
-//import toats
 import toast from "react-hot-toast";
-
-//import react-confirm-alert
 import { confirmAlert } from 'react-confirm-alert';
-
-//import CSS react-confirm-alert
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
-function CategoriesIndex() {
+function PlacesIndex() {
+  document.title = "Places - Administrator Travel GIS";
 
-  //title page
-  document.title = "Categories - Administrator Travel GIS";
-
-  //state posts
-  const [categories, setCategories] = useState([]);
-
-  //state currentPage
+  const [places, setPlaces] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-
-  //state perPage
   const [perPage, setPerPage] = useState(0);
-
-  //state total
-  const [total, setTotal] = useState(0);
-
-  //state search
   const [search, setSearch] = useState("");
-
-  //token
+  const [total, setTotal] = useState(0);
   const token = Cookies.get("token");
 
-  //function "fetchData"
   const fetchData = async (pageNumber, searchData) => {
-
-    //define variable "page"
     const page = pageNumber ? pageNumber : currentPage;
-
-    //define variable "searchQuery"
     const searchQuery = searchData ? searchData : search;
 
-    //fetching data from Rest API
-    await Api.get(`/api/admin/categories?q=${searchQuery}&page=${page}`, {
+    await Api.get(`/api/admin/places?q=${searchQuery}&page=${page}`, {
       headers: {
-        //header Bearer + Token
-        Authorization: `Bearer ${token}`,
+
+        Authorization: `Bearer ${token}`
       }
-    })
-      .then(response => {
-        //set data response to state "categories"
-        setCategories(response.data.data.data);
-
-        //set currentPage
-        setCurrentPage(response.data.data.current_page);
-
-        //set perPage
-        setPerPage(response.data.data.per_page);
-
-        //total
-        setTotal(response.data.data.total);
-      });
+    }).then(response => {
+      setPlaces(response.data.data.data);
+      setCurrentPage(response.data.data.current_page);
+      setPerPage(response.data.data.per_page);
+      setTotal(response.data.data.total);
+    });
   };
 
   //hook
   useEffect(() => {
-    //call function "fetchData"
     fetchData();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  //function "searchHandler"
+  //function "handleSearch"
   const searchHandlder = (e) => {
     e.preventDefault();
 
-    //call function "fetchDataPost" with params
+    //call function "fetchDataPost"
     fetchData(1, search)
   }
 
-  //function "deleteCategory"
-  const deleteCategory = (id) => {
-
-    //show confirm alert
+  //function "handleDelete"
+  const deletePlace = (id) => {
     confirmAlert({
       title: 'Are You Sure ?',
       message: 'want to delete this data ?',
       buttons: [{
         label: 'YES',
         onClick: async () => {
-          await Api.delete(`/api/admin/categories/${id}`, {
+          await Api.delete(`/api/admin/places/${id}`, {
             headers: {
               //header Bearer + Token
               Authorization: `Bearer ${token}`,
@@ -144,58 +95,50 @@ function CategoriesIndex() {
           <div className="col-12">
             <div className="card border-0 rounded shadow-sm border-top-success">
               <div className="card-header">
-                <span className="font-weight-bold"><i className="fa fa-folder"></i> CATEGORIES</span>
+                <span className="font-weight-bold"><i className="fa fa-map-marked-alt"></i> PLACES</span>
               </div>
               <div className="card-body">
+
                 <form onSubmit={searchHandlder} className="form-group">
                   <div className="input-group mb-3">
-                    <Link to="/admin/categories/create" className="btn btn-md btn-success"><i className="fa fa-plus-circle"></i> ADD NEW</Link>
-                    <input type="text" className="form-control" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="search by category name" />
+                    <Link to="/admin/places/create" className="btn btn-md btn-success"><i className="fa fa-plus-circle"></i> ADD NEW</Link>
+                    <input type="text" className="form-control" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="search by place title" />
                     <button type="submit" className="btn btn-md btn-success"><i className="fa fa-search"></i> SEARCH</button>
                   </div>
                 </form>
+
                 <div className="table-responsive">
                   <table className="table table-bordered table-striped table-hovered">
                     <thead>
                       <tr>
-                        <th scope="col" width="5%">No.</th>
-                        <th scope="col">Image</th>
-                        <th scope="col">Category Name</th>
-                        <th scope="col" width="10%">Actions</th>
+                        <th scope="col">No.</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Actions</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {categories.map((category, index) => (
+                      {places.map((place, index) => (
                         <tr key={index}>
                           <td className="text-center">{++index + (currentPage - 1) * perPage}</td>
+                          <td>{place.title}</td>
+                          <td>{place.category.name}</td>
                           <td className="text-center">
-                            <img src={category.image} alt="" width="50" />
-                          </td>
-                          <td>{category.name}</td>
-                          <td className="text-center">
-                            <Link to={`/admin/categories/edit/${category.id}`} className="btn btn-sm btn-primary me-2"><i className="fa fa-pencil-alt"></i></Link>
-                            <button onClick={() => deleteCategory(category.id)} className="btn btn-sm btn-danger"><i className="fa fa-trash"></i></button>
+                            <button onClick={() => deletePlace(place.id)} className="btn btn-sm btn-danger"><i className="fa fa-trash"></i></button>
                           </td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
-                  <PaginationComponent
-                    currentPage={currentPage}
-                    perPage={perPage}
-                    total={total}
-                    onChange={(pageNumber) => fetchData(pageNumber)}
-                    position="end"
-                  />
                 </div>
+                <PaginationComponent currentPage={currentPage} perPage={perPage} total={total} onChange={(pageNumber) => fetchData(pageNumber)} position="end" />
               </div>
             </div>
           </div>
         </div>
       </LayoutAdmin>
     </React.Fragment>
-  )
-
+  );
 }
 
-export default CategoriesIndex
+export default PlacesIndex;
--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -11,6 +11,8 @@ import Dashboard from '../pages/admin/dashboard/Index'; //import view admin Dash
 import CategoriesIndex from '../pages/admin/categories/Index.jsx'; //import view admin categories
 import CategoryCreate from '../pages/admin/categories/Create.jsx'; //import view admin categories
 import CategoryEdit from "../pages/admin/categories/Edit.jsx"; //import view admin categories
+import PlacesIndex from '../pages/admin/places/index.jsx'; //import view admin placesq
+import PlaceCreate from '../pages/admin/places/Create.jsx'; //import view admin places
 
 function RoutesIndex() {
   return (
@@ -20,44 +22,22 @@ function RoutesIndex() {
       <Route path="/admin/login" element={<Login />} />
 
       {/* private route "/admin/dashboard" */}
-      <Route
-        path="/admin/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/admin/dashboard" element={ <PrivateRoute> <Dashboard /> </PrivateRoute>}/>
 
       {/* private route "/admin/categories" */}
-      <Route
-        path="/admin/categories"
-        element={
-          <PrivateRoute>
-            <CategoriesIndex />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/admin/categories" element={ <PrivateRoute> <CategoriesIndex /> </PrivateRoute> }/>
 
       {/* private route "/admin/categories/create" */}
-      <Route
-        path="/admin/categories/create"
-        element={
-          <PrivateRoute>
-            <CategoryCreate />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/admin/categories/create" element={ <PrivateRoute> <CategoryCreate /> </PrivateRoute> } />
 
       {/* private route "/admin/categories/edit/:id" */}
-      <Route
-        path="/admin/categories/edit/:id"
-        element={
-          <PrivateRoute>
-            <CategoryEdit />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/admin/categories/edit/:id" element={ <PrivateRoute> <CategoryEdit /> </PrivateRoute> } />
+
+      {/* private route "/admin/places" */}
+      <Route path="/admin/places" element={ <PrivateRoute> <PlacesIndex /> </PrivateRoute> } />
+
+      {/* private route "/admin/places/create" */}
+      <Route path="/admin/places/create" element={ <PrivateRoute> <PlaceCreate /> </PrivateRoute> } />
     </Routes>
   )
 }
