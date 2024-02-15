85a8e6dd9bd6fee03d8d57c82465cbf79d2affb3
Reno <144994054+RenoArdiansyah@users.noreply.github.com>
2024-02-15 17:13:04 +0700

    add category edit in api admin

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from "./PrivateRoutes"; //import component private routes
 import Dashboard from '../pages/admin/dashboard/Index'; //import view admin Dashboard
 import CategoriesIndex from '../pages/admin/categories/Index.jsx'; //import view admin categories
 import CategoryCreate from '../pages/admin/categories/Create.jsx'; //import view admin categories
+import CategoryEdit from "../pages/admin/categories/Edit.jsx"; //import view admin categories
 
 function RoutesIndex() {
   return (
@@ -44,6 +45,16 @@ function RoutesIndex() {
         element={
           <PrivateRoute>
             <CategoryCreate />
+          </PrivateRoute>
+        }
+      />
+
+      {/* private route "/admin/categories/edit/:id" */}
+      <Route
+        path="/admin/categories/edit/:id"
+        element={
+          <PrivateRoute>
+            <CategoryEdit />
           </PrivateRoute>
         }
       />
