8d2e0b1e15e23785a641c1f76c28c4e95ee4013e
Reno <144994054+RenoArdiansyah@users.noreply.github.com>
2024-02-15 16:12:05 +0700

    add index and delete category api admin

--- src/Pages/admin/dashboard/Index.jsx
+++ src/Pages/admin/dashboard/Index.jsx
@@ -5,7 +5,7 @@ import Cookies from "js-cookie"; //import js cookie
 
 function Dashboard() {
     document.title = "Dashboard - Administrator Travel GIS"; //title page
-    
+
     //set state
     const [categories, setCategories] = useState(0);
     const [places, setPlaces] = useState(0);
@@ -49,75 +49,76 @@ function Dashboard() {
     return (
         <React.Fragment>
             <LayoutAdmin>
-            <div className="row mt-4">
+                <div className="row mt-4">
                     <div className="col-12">
                         <div className="card border-0 rounded shadow-sm border-top-success">
                             <div className="card-header">
                                 <span className="font-weight-bold"><i className="fa fa-tachometer-alt"></i> DASHBOARD</span>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row mt-4">
-                    <div className="col-12 col-lg-3 mb-4">
-                    <div className="card border-0 shadow-sm overflow-hidden">
-                        <div className="card-body p-0 d-flex align-items-center">
-                            <div className="bg-primary py-4 px-5 mfe-3" style={{ width: "130px" }}>
-                                <i className="fas fa-folder fa-2x text-white"></i>
+
+                        <div className="row mt-4">
+                            <div className="col-12 col-lg-3 mb-4">
+                                <div className="card border-0 shadow-sm overflow-hidden">
+                                    <div className="card-body p-0 d-flex align-items-center">
+                                        <div className="bg-primary py-4 px-5 mfe-3" style={{ width: "130px" }}>
+                                            <i className="fas fa-folder fa-2x text-white"></i>
+                                        </div>
+                                        <div>
+                                            <div className="text-value text-primary">{categories}</div>
+                                            <div className="text-muted text-uppercase font-weight-bold small">
+                                                CATEGORIES
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <div className="text-value text-primary">{categories}</div>
-                                <div className="text-muted text-uppercase font-weight-bold small">
-                                    CATEGORIES
+                            <div className="col-12 col-lg-3 mb-4">
+                                <div className="card border-0 rounded shadow-sm overflow-hidden">
+                                    <div className="card-body p-0 d-flex align-items-center">
+                                        <div className="bg-success py-4 px-5 mfe-3" style={{ width: "130px" }}>
+                                            <i className="fas fa-map-marked-alt fa-2x text-white"></i>
+                                        </div>
+                                        <div>
+                                            <div className="text-value text-success">{places}</div>
+                                            <div className="text-muted text-uppercase font-weight-bold small">
+                                                PLACES
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-lg-3 mb-4">
+                                <div className="card border-0 rounded shadow-sm overflow-hidden">
+                                    <div className="card-body p-0 d-flex align-items-center">
+                                        <div className="bg-warning py-4 px-5 mfe-3" style={{ width: "130px" }}>
+                                            <i className="fas fa-images fa-2x text-white"></i>
+                                        </div>
+                                        <div>
+                                            <div className="text-value text-warning">{sliders}</div>
+                                            <div className="text-muted text-uppercase font-weight-bold small">
+                                                SLIDERS
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-lg-3 mb-4">
+                                <div className="card border-0 rounded shadow-sm overflow-hidden">
+                                    <div className="card-body p-0 d-flex align-items-center">
+                                        <div className="bg-danger py-4 px-5 mfe-3" style={{ width: "130px" }}>
+                                            <i className="fas fa-users fa-2x text-white"></i>
+                                        </div>
+                                        <div>
+                                            <div className="text-value text-danger">{users}</div>
+                                            <div className="text-muted text-uppercase font-weight-bold small">
+                                                USERS
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-4">
-                    <div className="card border-0 rounded shadow-sm overflow-hidden">
-                        <div className="card-body p-0 d-flex align-items-center">
-                            <div className="bg-success py-4 px-5 mfe-3" style={{ width: "130px" }}>
-                                <i className="fas fa-map-marked-alt fa-2x text-white"></i>
-                            </div>
-                            <div>
-                                <div className="text-value text-success">{places}</div>
-                                <div className="text-muted text-uppercase font-weight-bold small">
-                                    PLACES
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-4">
-                    <div className="card border-0 rounded shadow-sm overflow-hidden">
-                        <div className="card-body p-0 d-flex align-items-center">
-                            <div className="bg-warning py-4 px-5 mfe-3" style={{ width: "130px" }}>
-                                <i className="fas fa-images fa-2x text-white"></i>
-                            </div>
-                            <div>
-                                <div className="text-value text-warning">{sliders}</div>
-                                <div className="text-muted text-uppercase font-weight-bold small">
-                                    SLIDERS
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-4">
-                    <div className="card border-0 rounded shadow-sm overflow-hidden">
-                        <div className="card-body p-0 d-flex align-items-center">
-                            <div className="bg-danger py-4 px-5 mfe-3" style={{ width: "130px" }}>
-                                <i className="fas fa-users fa-2x text-white"></i>
-                            </div>
-                            <div>
-                                <div className="text-value text-danger">{users}</div>
-                                <div className="text-muted text-uppercase font-weight-bold small">
-                                    USERS
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </LayoutAdmin>
